102165478d30525b6684f38a9e922139fa1d3a9d
Alex Jorolemon <jjorole1@gmail.com>
2022-02-16 15:36:07 -0500

    made everything public and added interface

--- Slorpus/EnemyBullet.cs
+++ Slorpus/EnemyBullet.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Slorpus
 {
-    struct EnemyBullet
+    public struct EnemyBullet
     {
     }
 }
--- Slorpus/Wall.cs
+++ Slorpus/Wall.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Slorpus
 {
-    struct Wall
+    public struct Wall
     {
     }
 }
