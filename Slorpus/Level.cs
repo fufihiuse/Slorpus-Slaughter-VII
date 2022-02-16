e214dadae595191b2ac902e512d0b262e75c7911
Jackson Majewski <jdm1631@rit.edu>
2022-02-16 17:47:08 -0500

    Added support to load levels from file

--- Slorpus/Level.cs
+++ Slorpus/Level.cs
@@ -12,11 +12,71 @@ namespace Slorpus
     public class Level
     {
         //Fields
+        private char[,] level;
         private Wall[,] walls;
+
+        //Constructor
+        public Level()
+        {
+
+        }
 
         //Methods
         public void LoadFromFile(string filepath)
         {
+            string line;
+            string[] data;
+            /*
+             * Load file into level array
+             * TODO: Loop through level, check if I or W, if so load into walls array
+             * 
+             */
+
+            //Loading from file
+            try
+            {
+                //Attempt to load file
+                StreamReader input = new StreamReader(filepath);
+
+                //Get number of rows and columns
+                line = input.ReadLine();
+                data = line.Split(',');
+                level = new char[int.Parse(data[0]), int.Parse(data[1])];
+
+                //Load 2D array into level
+                line = input.ReadLine();
+                for (int row = 0; row < level.GetLength(0); row++)
+                {
+                    for (int column = 0; column < level.GetLength(1); column++)
+                    {
+                        level[row, column] = line[column];
+                        /*
+                        switch (line[column])
+                        {
+                            case 'W':
+                                walls[row, column] = new Wall();
+                                break;
+                            case 'I':
+                                walls[row, column] = new Wall();
+                                break;
+                            default:
+                                walls[row, column] = null;
+                        }
+                        *May not work due to walls array not accepting nulls (Wall is a struct)
+                        */
+                    }
+                    line = input.ReadLine();
+                }
+
+                input.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            //Loop through level, check if I or W, if so load into walls array
+            
 
         }
 
