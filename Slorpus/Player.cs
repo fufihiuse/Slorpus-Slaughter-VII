0d5d44382e246d6d4068f801668b16e1f3aa3e10
AngelosV03 <angelosv03minepoke@gmail.com>
2022-02-23 13:32:33 -0500

    ProcessKeyboardInput started

--- Slorpus/Player.cs
+++ Slorpus/Player.cs
@@ -14,11 +14,38 @@ namespace Slorpus
         Rectangle rectangle;
         int x;
         int y;
+        KeyboardState keyboardState;
 
         // Constructor
-        public Player(Point pos, Vector2 vel, int width, int height): base(pos, vel, width, height)
+        public Player(Point pos, Vector2 vel, int width, int height, Texture2D playerAsset): base(pos, vel, width, height)
         {
             rectangle = new Rectangle(x, y, width, height);
+            this.playerAsset = playerAsset;
+        }
+
+        // Method
+        /// <summary>
+        /// Processes the Keyboard Input and adds it to velocity
+        /// </summary>
+        public void ProcessKeyboardInput()
+        {
+            keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.W) == true)
+            {
+                
+            }
+            if (keyboardState.IsKeyDown(Keys.A) == true)
+            {
+
+            }
+            if (keyboardState.IsKeyDown(Keys.S) == true)
+            {
+
+            }
+            if (keyboardState.IsKeyDown(Keys.D) == true)
+            {
+
+            }
         }
     }
 }
