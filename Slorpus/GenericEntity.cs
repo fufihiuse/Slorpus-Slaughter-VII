74ed36d1b771b70532e6bc401d516e2e8f5d5d3b
Jackson Majewski <jacksonmajewski@gmail.com>
2022-03-08 18:55:01 -0500

    Added properties oop

--- Slorpus/GenericEntity.cs
+++ Slorpus/GenericEntity.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Slorpus
 {
+    /// <summary>
+    /// Struct to use for generic entity creation, holds a char and position
+    /// </summary>
     struct GenericEntity
     {
         //Fields
         private char entityType;
-        private int x;
-        private int y;
+        private Point position;
+
+        //Properties
+        public char EntityType
+        {
+            get { return entityType; }
+        }
+        public Point Position
+        {
+            get { return position; }
+        }
+        public int X
+        {
+            get { return position.X; }
+        }
+        public int Y
+        {
+            get { return position.Y; }
+        }
 
         //Constructor
         public GenericEntity(char entityType, int x, int y)
         {
             this.entityType = entityType;
-            this.x = x;
-            this.y = y;
+            this.position = new Point(x, y);
         }
     }
 }
