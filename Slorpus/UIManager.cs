fca0250e50e269c813b27ad916ab9fe5f59843a6
Alex Jorolemon <jjorole1@gmail.com>
2022-03-18 13:46:21 -0400

    push

--- Slorpus/UIManager.cs
+++ Slorpus/UIManager.cs
@@ -25,11 +25,77 @@ namespace Slorpus
     public class UIManager
     {
         //fields
+        GameState currentGameState;
+
+        //buttons
+        List<Button> menuButtons;
+        List<Button> gameButtons;
+        List<Button> gameOverButtons;
+        List<Button> pauseButtons;
+        List<Button> settingsButtons;
 
         //properties
+        public GameState CurrentGameState
+        {
+            get
+            {
+                return currentGameState;
+            }
+        }
 
         //constructor
+        public UIManager()
+        {
+            currentGameState = GameState.Menu;
+        }
 
         //methods
+        /// <summary>
+        /// loads all the textures for the buttons and then passes them into  the coresponding buttons
+        /// </summary>
+        public void LoadButtons()
+        {
+
+        }
+        /// <summary>
+        /// updates the ui gamestate depending on which button is pressed
+        /// </summary>
+        public void Update(MouseState ms)
+        {
+            switch (currentGameState)
+            {
+                case GameState.Menu:
+                    break;
+
+                case GameState.GameOver:
+                    break;
+
+                case GameState.Pause:
+                    break;
+
+                case GameState.Settings:
+                    break;
+            }
+        }
+        /// <summary>
+        /// draws all UI
+        /// </summary>
+        public void Draw()
+        {
+            switch (currentGameState)
+            {
+                case GameState.Menu:
+                    break;
+
+                case GameState.GameOver:
+                    break;
+
+                case GameState.Pause:
+                    break;
+
+                case GameState.Settings:
+                    break;
+            }
+        }
     }
 }
