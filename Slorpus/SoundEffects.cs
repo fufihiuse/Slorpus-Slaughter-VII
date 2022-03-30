343010161c5d4da898ca4c0f8b26cdec300e5186
Angelos Veloudos <angelosv03minepoke@gmail.com>
2022-03-30 15:03:37 -0400

    Added comments

--- Slorpus/SoundEffects.cs
+++ Slorpus/SoundEffects.cs
@@ -9,6 +9,9 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Slorpus
 {
+    /// <summary>
+    /// Holds a list of SoundEffects that are used based on different situations in the game
+    /// </summary>
     class SoundEffects
     {
         // Field
@@ -16,18 +19,28 @@ namespace Slorpus
         static List<SoundEffect> soundEffectsBank;
 
         // Constructor
+        /// <summary>
+        /// Creates a new SoundEffects object and declares a new list that can be filled with SoundEffects
+        /// </summary>
         public SoundEffects()
         {
             soundEffectsBank = new List<SoundEffect>();
-            //soundEffectsBank.Add(content.Load<SoundEffect>("Bullet Noise"));
         }
 
-        // Method
+        // Methods
+        /// <summary>
+        /// Adds each of the sounds needed in the game
+        /// </summary>
+        /// <param name="content"></param>
         public static void AddSounds(Microsoft.Xna.Framework.Content.ContentManager content)
         {
             soundEffectsBank.Add(content.Load<SoundEffect>("Bullet Noise"));
         }
 
+        /// <summary>
+        /// Plays a specific sound in the list decided by the parameter
+        /// </summary>
+        /// <param name="index"></param>
         public static void PlayEffect(int index)
         {
             soundEffectsBank[index].Play();
