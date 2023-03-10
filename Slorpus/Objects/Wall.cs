be60f34bfd5bcc27aba8666ff0e9e99d2fc30d27
Ian Mcfarlane <i.mcfarlane2002@gmail.com>
2023-03-10 03:01:38 -0800

    new collision function complete

--- Slorpus/Objects/Wall.cs
+++ Slorpus/Objects/Wall.cs
@@ -40,9 +40,33 @@ namespace Slorpus.Objects
             Vector2 min = new Vector2(Math.Max(hitbox.Left, position.Left), Math.Max(hitbox.Top, position.Top));
             Vector2 max = new Vector2(Math.Min(hitbox.Right, position.Right), Math.Min(hitbox.Bottom, position.Bottom));
             bool collided = min.X < max.X && min.Y < max.Y;
-
+            
+            // also get depth and normal if necessary
             Vector2 depth = new Vector2(0.0f);
             Vector2 normal = new Vector2(0.0f);
+
+            if (collided)
+            {
+                // calculate depth
+                depth = max - min;
+                
+                // make normal components signs of depth
+                if (depth.Y != 0)
+                {
+                    // if depth.x > depth.y, then the collision is more horizontal and the normal's
+                    // Y component should be the non-zero sign of depth.Y (normal pointing either up or down).
+                    normal.Y = (depth.X >= depth.Y) ? (depth.Y / Math.Abs(depth.Y)) : 0;
+                }
+                if (depth.X != 0)
+                {
+                    // same theory: if the collision is more vertical, then we need to correct horizontally
+                    // so the normal points as the sign of the depth
+                    normal.X = (depth.X <= depth.Y) ? (depth.X / Math.Abs(depth.X)) : 0;
+                }
+                normal.X *= ((hitbox.Left > position.Left) ? 2 : 0) - 1;
+                normal.Y *= ((hitbox.Top > position.Top) ? 2 : 0) - 1;
+                normal.Normalize();
+            }
 
             return new CollisionInfo(collided, depth, normal);
         }
